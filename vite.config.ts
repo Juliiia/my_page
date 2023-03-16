9388ae0bd6220c2ca22fff2f8cec0c4b547484ad
Julia Krause <jk.hohnstein@hotmail.com>
2023-03-16 07:41:23 +0100

    change path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 // Similar Problem https://github.com/vitejs/vite/issues/3533
 export default defineConfig({
-  base: 'juliamucha.art/',
+  base: '/',
   plugins: [react()],
 })
